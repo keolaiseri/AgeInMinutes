3ab074a897cb108550b1895cbce7f254319fa359
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-10 11:15:24 -1000

    Added Button

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -2,15 +2,19 @@ package com.example.ageinminutes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    
+    val btnDatePicker = findViewById<Button>(R.id.btnDatePicker)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnDatePicker.setOnClickListener {
+            Toast.makeText(this,"Button Works", Toast.LENGTH_LONG).show() }
 
 
 
